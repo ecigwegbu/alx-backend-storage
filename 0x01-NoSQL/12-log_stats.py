48bef17fb372a4893c5bd67852e55ce18fdece3f
ecigwegbu <igwegbu@gmail.com>
2023-07-18 21:20:55 +0000

    12-logs

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,36 +1,9 @@
 #!/usr/bin/env python3
-"""
-    Write a Python script that provides some stats about Nginx logs
-    stored in MongoDB:
-
-    Database: logs
-    Collection: nginx
-    Display (same as the example):
-    first line: x logs where x is the number of documents in this collection
-    second line: Methods:
-    5 lines with the number of documents with the method = ["GET", "POST",
-    "PUT", "PATCH", "DELETE"] in this order (see example below - warning:
-    it's a tabulation before each line)
-    one line with the number of documents with:
-    method=GET
-    path=/status
-    You can use this dump as data sample: dump.zip
-
-    The output of your script must be exactly the same as the example
-"""
+"""12. Log Stats - provide some stats about a Mongo database dump"""
 import pymongo
-from pymongo import MongoClient
 
 
-# setup workspace
-logdata = MongoClient().logs.nginx
-
-# print header
-print('{} logs'.format(logdata.count_documents({})))
-print('Methods:')
-
-# loop over all documents
-
+pass
 
 if __name__ == '__main__':
     pass
