8a753fe70c34b0cab6d0a63a2dbe4c3abd8fbae5
ecigwegbu <igwegbu@gmail.com>
2023-07-18 20:03:17 +0000

    11. Where can I learn Python?

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -13,6 +13,4 @@ def update_topics(mongo_collection, name, topics):
     topics (list of strings) will be the list of topics approached in the
     school
     """
-    # mongo_collection.delete_many({"name": name})
     mongo_collection.update_many({"name": name}, {'$set': {'topics': topics}})
-    #                             upsert=True)
