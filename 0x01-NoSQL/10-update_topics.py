ae15a5dd605afd82f5287bba129a391115c90327
ecigwegbu <igwegbu@gmail.com>
2023-07-18 18:20:10 +0000

    10 - update only

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -13,6 +13,6 @@ def update_topics(mongo_collection, name, topics):
     topics (list of strings) will be the list of topics approached in the
     school
     """
-    mongo_collection.delete_many({"name": name})
-    # mongo_collection.update_one({"name": name}, {'$set': {'topics': topics}},
+    # mongo_collection.delete_many({"name": name})
+    mongo_collection.update_one({"name": name}, {'$set': {'topics': topics}})
     #                             upsert=True)
