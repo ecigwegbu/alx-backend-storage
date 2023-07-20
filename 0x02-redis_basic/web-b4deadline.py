804b7d20cff85a2b19c99ecb6e98901a1944576a
ecigwegbu <igwegbu@gmail.com>
2023-07-20 14:51:43 +0000

    cleanup

---
+++ 0x02-redis_basic/web-b4deadline.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""5. Implement expiring web cache and tracker"""
+import requests
+import redis
+
+
+def get_page(url: str) -> str:
+    """Return the URL of a web page and track number of visits
+    with an expiration of 10 seconds"""
+    # url = "https://www.google.com"
+    response = requests.get(url)
+    r = redis.Redis()
+    count = r.incr("count:{url}")
+    r.expire("count:{url}", 10)
+    return response.text
+
+
+if __name__ == '__main__':
+    url = "https://www.google.com"
+    page = get_page(url)
+    r = redis.Redis()
+    print('Visited: ', int(r.get("count:{url}")))
--- 0x02-redis_basic/web-wrapper.py
+++ 0x02-redis_basic/web-wrapper.py
@@ -2,12 +2,11 @@
 """5. Implement expiring web cache and tracker"""
 import requests
 import redis
-import typing
 
 
-def get_page_decorator(get_page):
-    def wrapper():
-        gt_page()
+def get_page_decorator(gt_page):
+    def wrapper(url):
+        gt_page(url)
         r = redis.Redis()
         count = r.incr("count:{url}")
         r.expire("count:{url}", 10)
@@ -19,12 +18,13 @@ def get_page(url: str) -> str:
     """Return the URL of a web page"""
     # url = "https://www.google.com"
     response = requests.get(url)
+    print(response.text)
     return response.text
 
 
 url = "https://www.google.com"
 print(get_page(url))
-r = redis.redis()
+r = redis.Redis()
 print('Visited: ', int(r.get("count:{url}")))
 
 
